13d3462fc82b362d9081deb3e9053ac30ef62a38
merveturhan <merveturhan@gmail.com>
2023-07-16 15:12:42 +0300

    Search,Dark mode,Header, Footer

--- template.js
+++ template.js
@@ -1,6 +1,25 @@
 //Dark Mode
 let darkModu=()=>{
-    alert("Dark modu");
+  //  alert("Dark modu");
+    window.document.classList.toggle("dark_mode")
 }
 
-wimdow.alert ("Deneme");
+//input search
+$(document).ready(function(){
+    const searchApi=["Adana","Balikesir","Çorum","Denizli","Diyarbakır","Edirne","Malatya","Van"];
+    $("#tags").autocomplete({
+        source:searchApi
+    })
+    }); //end
+
+    //Footer
+let newDate=()=>{
+    const date=new Date().getFullYear();
+    //  JS DOM
+    //document.getElementById("date_id").innerHTML=new Date().getFullYear();
+
+  //Jquery Dom
+  $("#date_id").html(date)
+}
+    newDate()
+
