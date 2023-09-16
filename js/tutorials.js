9972356961c431da219437defd9e7b373fba6a98
merveturhan <merveturhan@gmail.com>
2023-09-16 17:22:49 +0300

    javascript

---
+++ js/tutorials.js
@@ -0,0 +1,13 @@
+//single comment
+/*
+multiple comment
+*/
+
+console.log("loglama");
+console.info("info");
+console.warn("warn");
+console.error("error");
+alert("pop-up bilgilendirme-1");
+window.alert("pop-up bilgilendirme-2");
+
+document.writeln("ekranda yazı");
