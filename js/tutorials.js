07506994129ab30de5c0b2f9728f823697e1b76e
merveturhan <merveturhan@gmail.com>
2024-03-14 15:45:14 +0300

    loop

--- js/tutorials.js
+++ js/tutorials.js
@@ -58,12 +58,12 @@ result2=56;
 var result2; */
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Operators
-// + - * ** / %
-// ! DEĞİL &&=VE ||=VEYA
-// X++ İŞLEMDEN SONRA BİR ATTIR
-//X-- İŞLEMDEN SONRA BİR AZALT
-//++X İŞLEMDEN ÖNCE BİR ARTTIR SONRA İŞLEM
-//--X İŞLEMDEN BİR ÖNCE AZALT SONRA İŞLEME DEVAM ET
+// // + - * ** / %
+// //  ! DEĞİL &&=VE ||=VEYA
+// // X++ İŞLEMDEN SONRA BİR ATTIR
+// //X-- İŞLEMDEN SONRA BİR AZALT
+// //++X İŞLEMDEN ÖNCE BİR ARTTIR SONRA İŞLEM
+// //--X İŞLEMDEN BİR ÖNCE AZALT SONRA İŞLEME DEVAM ET
 
 // = ATAMA
 // == EŞİT (TÜRÜNE BAKMA)
@@ -242,13 +242,118 @@ console.log(Math.round(Math.random()*9+1)); */
 //let veya const
 //arrow function
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// function deneme1(){
+//     console.log("normal function");
+// }
+// deneme1();
+
+// let deneme2=function(){
+//     console.log("anonymous function");
+// }
+// deneme2();
+
+// const deneme3=()=>{
+//     console.log("arrow function");
+// }
+// deneme3();
+
+
+
 //Immedia Function()();
+//   (function(){
+//     console.log("deneme4");
+//   })(); // main dizi yapılarında kullanıllıyor çok kullanılmıyor ama
+
+////////////////////////////////////////////////////////////////////
 //condition
+ const condi=()=>{
+    let number=4;
+    if(number>=2){
+        console.log("Büyüktür");
+    } else{
+        console.log("Küçüktür");
+    }
+ }
+ condi();
+
+
+ const condi2=()=>{
+    let number=4;
+    if(number>=2)
+        console.log("Büyüktür");
+    else
+        console.log("Küçüktür");
+    
+ }
+ condi2();
+
+
+ const condi3=()=>{
+    let number=4;
+    let result=(number>=2) ? "Büyüktür":"Küçüktür";
+     console.log(result);
+ }
+ condi3();
+
+ 
+ const condi4=()=>{
+    let number=4;
+     console.log((number>=2) ? "Büyüktür":"Küçüktür");
+ }
+ condi4();
+
+ const condi5=()=>{
+    let number=4;
+    if(number=="1"){
+        console.log("1");
+    } else if(number==2){
+        console.log("2");
+    }else if(number==3){
+        console.log("3");
+ }else if(number==4){
+    console.log("4");
+}else{
+    console.log("1<X<5 Dışındadır");
+} }
+ condi5();
+
+
+ const condi6=()=>{
+    let number=4;
+// break : döngü kırmak
+//return :metot kırmak
+//continue :1 kereye mahsus es geç sonra döngüye devam
+switch (number) {
+    case 1:
+        console.log("1");
+        break; // döngüyü kırmak
+    case 2:
+            console.log("2");
+            break;
+    case 3:
+        console.log("3");
+        break;
+     case 4:
+        console.log("4");
+        break;
+    default:
+        console.log("1<X<5 Dışındadır");
+        break;
+}
+ }
+condi6();
+
+
+
+
+
+
 //loop
 //örnekler
 
 
---------------------------------------------
+
 // let userData=prompt("Lütfen bir şeyler yazınız") ;
 // console.log(userData);
 //  console.log(userData.length);
@@ -265,4 +370,58 @@ console.log(Math.round(Math.random()*9+1)); */
 // console.log(userData.endsWith());
 // console.log(userData.charAt(1));
 // console.log(userData.substring(0,4)); 
----------------------------------------------------
+
+//ÖDEV:
+//Kullanıcı tarafından girilen bir sayının negatif mi, pozitif mi
+//(dikkat: normal function ile yazınız)
+
+// Kullanıcı tarafından girilen bir sayının tek mi, çift mi olduğunu gösteren
+//(dikkat:anonymous function ve ternary ile yazınız)
+
+let isOddEven=()=>{
+    const result=4;
+    if(result%2==0)
+    console.log("Çift"); 
+    else
+    console.log("Tektir")
+}
+isOddEven() //bölümünde kalan denilince bu akla gelsin
+
+//kullanıcadan alınan sayıya göre haftanın günlerini switch-case ile
+//Örneğin:kullanıcı 1 girdi: 1=pazartesi
+
+
+//////////////////////////////////////////////////////////////////
+
+//loop
+ 
+let loop=()=>{
+for (let i = 1; i <= 10; i++) {
+    document.write(i+ " ");
+}   
+}
+loop()
+document.writeln("<br/>")
+
+let loop2=()=>{
+    let i=1;
+    while(i <= 10) {
+
+            document.write(i+ " ");
+            i++;
+    }   
+    }
+    loop2();
+     document.writeln("<br/>")
+
+    let loop3=()=>{
+        let i=1;
+        do{
+            document.write(i+ " ");
+                i++;
+        }   
+        while(i <= 10) ;
+        }
+        loop3();
+
+//örnekler
